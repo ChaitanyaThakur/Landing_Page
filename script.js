840bf4b50158a595beae3194bf75e9112e804718
ChaitanyaThakur <97513118+ChaitanyaThakur@users.noreply.github.com>
2023-05-18 22:39:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+//Single Page
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+  
+    // Original:
+    // const sectionTop = current.offsetTop - 50;
+    //  
+    // update:
+    // Updated original line (above) to use getBoundingClientRect instead of offsetTop, based on:
+    // https://plainjs.com/javascript/styles/get-the-position-of-an-element-relative-to-the-document-24/
+    // https://newbedev.com/difference-between-getboundingclientrect-top-and-offsettop
+    // This allows the use of sections inside a relative parent, which I'm not using here, but needed for a project
+    //
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+    let sectionId = current.getAttribute("id");
+    console.log(sectionId);
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
+
+
+
+// PIE CHART 
+
+var xValues = ["Canva", "Sony Vegas Pro", "Camtasia", "FL Studio", "HTML", "CSS", "Bootstrap", "JavaScript", "ReactJs", "NodeJs", "Java", "SQL", "MondoDB", "RestAPI"];
+var yValues = [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145",
+  "#3D0000",
+  "#950101",
+  "#FF0000",
+  "#FF74B1",
+  "#FFA1CF",
+  "#FFD6EC",
+  "#FFB9B9",
+  "#BAD1C2",
+  "#4FA095"
+];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Skills"
+    }
+  }
+});
+
+
